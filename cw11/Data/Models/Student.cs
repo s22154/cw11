ec41e32e57020af4469955206af9ec01aa05814c
Dynia <piotr.dynia@gmail.com>
2022-06-20 14:25:38 +0200

    cw11

---
+++ cw11/Data/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw11.Data.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        public string Studies { get; set; }
+    }
+}
